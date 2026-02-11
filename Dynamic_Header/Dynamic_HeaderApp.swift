1736ef3106ad0cec7f01e12ab4789a047edb2040
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2026-02-11 11:41:07 +0530

    Initial Commit

---
+++ Dynamic_Header/Dynamic_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_HeaderApp.swift
+//  Dynamic_Header
+//
+//  Created by Sai Krishna on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
